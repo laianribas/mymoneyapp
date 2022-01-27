f173fa27ae1c198ef50ba0e138d68d11fcb1cf60
laianribas <laian67a@gmail.com>
2022-01-27 12:06:33 -0300

    Backend pronto

---
+++ backend/src/config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+module.exports = mongoose.connect('mongodb://localhost/mymoney', {
+    useNewUrlParser: true
+})
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório!"
+mongoose.Error.messages.Number.min =
+    "O '{VALUE}' informado é menor do que o valor mínimo de'{MIN}'."
+mongoose.Error.messages.Number.max =
+    "O '{VALUE}' informado é maior do que o valor máximo de'{MAX}'."
+mongoose.Error.messages.String.enum =
+    "'{VALUE}' não é válido para o atributo '{PATH}'."
