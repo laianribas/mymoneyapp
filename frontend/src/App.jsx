5d01c531b0d862c55fbebabc302accb759c38cd0
laianribas <laian67a@gmail.com>
2022-01-28 12:52:16 -0300

    adiciona adminlte

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Home from './components/Home'
+import SideNav from './components/SideNav'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <SideNav />
+    </>
+  )
+}
+
+export default App
