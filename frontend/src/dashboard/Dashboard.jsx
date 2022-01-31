f531904f47c5d0cb3dbad78f8fbb4b3dcbecc919
laianribas <laian67a@gmail.com>
2022-01-30 21:30:08 -0300

    criação dos valueBox

--- frontend/src/dashboard/Dashboard.jsx
+++ frontend/src/dashboard/Dashboard.jsx
@@ -1,13 +1,40 @@
 import React, { Component } from 'react'
 import Content from '../common/template/Content'
 import ContentHeader from '../common/template/ContentHeader'
+import ValueBox from '../common/widget/valueBox'
 
 export class Dashboard extends Component {
   render() {
     return (
       <div>
         <ContentHeader title="Dashboard" subTitle="version 1.0.0" />
-        <Content>Dashboard</Content>
+        <Content>
+          <div className="container-fluid">
+            <div className="row">
+              <ValueBox
+                cols="12 4"
+                color="green"
+                icon="piggy-bank"
+                value="R$ 10,00"
+                text="Totela de créditos"
+              />
+              <ValueBox
+                cols="12 4"
+                color="red"
+                icon="credit-card"
+                value="R$ 10,00"
+                text="Totela de débitos"
+              />
+              <ValueBox
+                cols="12 4"
+                color="blue"
+                icon="funnel-dollar"
+                value="R$ 0,00"
+                text="Valor Consolidado"
+              />
+            </div>
+          </div>
+        </Content>
       </div>
     )
   }
