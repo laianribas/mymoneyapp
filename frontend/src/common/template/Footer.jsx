686e1086df0de00bb017b7f1079e7d2e552da4c3
laianribas <laian67a@gmail.com>
2022-02-01 20:35:39 -0300

    Footer

--- frontend/src/common/template/Footer.jsx
+++ frontend/src/common/template/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
           target="_blank"
           rel="noreferrer"
         >
-          Laian Ribas
+          Laian Ribas Nascimento
         </a>
         .
       </strong>
